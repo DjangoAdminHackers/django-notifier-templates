77e8d6331e929413358c31a5856218ee0ff5f9c0
Andy Baker <andy@andybak.net>
2014-09-22 19:48:25 +0100

    fix timezone warning

--- notifier_templates/management/commands/scheduled_notifier_emails.py
+++ notifier_templates/management/commands/scheduled_notifier_emails.py
@@ -1,13 +1,10 @@
-import os
-from datetime import date, datetime
+from time import timezone
 
 from django.core.management.base import BaseCommand
-from django.conf import settings
-from django.db.models import FileField
 from django.db import models
 
 from notifier_templates.models import HasNotifiers
-from notifier_templates.utils import send_html_email, generate_email_html, do_notify
+
 
 class Command(BaseCommand):
     help = "Send scheduled notifier emails. "
@@ -24,9 +21,9 @@ class Command(BaseCommand):
                         date_filters = {}
                         min_timedelta, max_timedelta = rules.get('min_timedelta', None), rules.get('max_timedelta', None) 
                         if min_timedelta:
-                            date_filters[rules['date_field'] + '__gte'] = datetime.now() + min_timedelta
+                            date_filters[rules['date_field'] + '__gte'] = timezone.now() + min_timedelta
                         if max_timedelta:
-                            date_filters[rules['date_field'] + '__lte'] = datetime.now() + max_timedelta
+                            date_filters[rules['date_field'] + '__lte'] = timezone.now() + max_timedelta
                         candidates = candidates.filter(**date_filters)
                         #make sure email had not been sent before. 
                         sent_objects_ids = [values[0] for values in model.get_sent_notifications(action).values_list('object_id')]
