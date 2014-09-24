2bb6492835dacd98283e3cf7f360c35d025720a3
Andy Baker <andy@andybak.net>
2014-09-24 14:41:48 +0100

    better template names

--- notifier_example/shop/models.py
+++ notifier_example/shop/models.py
@@ -16,14 +16,14 @@ class Invoice(models.Model, HasNotifiers):
     )
 
     NOTIFIER_TYPES = Choices(
-        'invoice_to_client',
-        'invoice_7_day_pre_due',
-        'invoice_1_day_pre_due',
+        'invoice_to_customer',
+        'invoice_7_days_before_due',
+        'invoice_1_day_before_due',
     )
     
     NOTIFIER_EVENTS = {
-        NOTIFIER_TYPES.invoice_1_day_pre_due: {'date_field': 'due', 'min_timedelta': timedelta(days=0), 'max_timedelta': timedelta(days=1), 'filters': {'status': STATUS.sent}, },
-        NOTIFIER_TYPES.invoice_7_day_pre_due: {'date_field': 'due', 'min_timedelta': timedelta(days=0), 'max_timedelta': timedelta(days=7), 'filters': {'status': STATUS.sent}, },
+        NOTIFIER_TYPES.invoice_7_days_before_due: {'date_field': 'due', 'min_timedelta': timedelta(days=0), 'max_timedelta': timedelta(days=1), 'filters': {'status': STATUS.sent}, },
+        NOTIFIER_TYPES.invoice_1_day_before_due: {'date_field': 'due', 'min_timedelta': timedelta(days=0), 'max_timedelta': timedelta(days=7), 'filters': {'status': STATUS.sent}, },
     }
 
     due = models.DateField(null=True, blank=True)
@@ -31,9 +31,7 @@ class Invoice(models.Model, HasNotifiers):
     
     def get_notifier_actions(self):
         return Choices(
-            (self.NOTIFIER_TYPES.invoice_to_client, 'Send invoice'),
-            (self.NOTIFIER_TYPES.invoice_7_day_pre_due, 'Send invoice 7 days due'),
-            (self.NOTIFIER_TYPES.invoice_1_day_pre_due, 'Send invoice 1 day due'),
+            (self.NOTIFIER_TYPES.invoice_to_customer, 'Send invoice'),
         )
 
     def get_notifier_recipients(self, action):
