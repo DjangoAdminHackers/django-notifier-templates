b34946a23605f3211120c69bf81cb9f171675307
Andy Baker <andy@andybak.net>
2014-09-30 12:08:51 +0100

    set hard requirements in setup.py install_requires

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='django-notifier-templates',
-      version='0.1',
-      description='django notifier templates',
-      author='Andy Baker',
-      author_email='andy@andybak.net',
-      packages=find_packages(),
-      package_data={
-          'notifier_templates': [
+setup(
+    name='django-notifier-templates',
+    version='0.1',
+    description='django notifier templates',
+    author='Andy Baker',
+    install_requires=[
+        'django-multi-email-field>=0.1,<1.0',
+        'django-dbsettings>=0.7,<1.0',
+    ],
+    author_email='andy@andybak.net',
+    packages=find_packages(),
+    package_data={
+        'notifier_templates': [
             'templates/*.html',
             'static/*',
-          ]
-      },
-      include_package_data=True,
+        ]
+    },
+    include_package_data=True,
 )
