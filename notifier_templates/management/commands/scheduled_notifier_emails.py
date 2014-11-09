11228d475f5586ea33de3873aba76da08f994738
fruitschen <fruitschen@gmail.com>
2014-11-10 00:10:55 +0800

    remove code duplication

--- notifier_templates/management/commands/scheduled_notifier_emails.py
+++ notifier_templates/management/commands/scheduled_notifier_emails.py
@@ -17,31 +17,7 @@ class Command(BaseCommand):
                 has_events = hasattr(model, 'NOTIFIER_EVENTS')
                 if is_subclass and has_events:
                     for action, rules in model.NOTIFIER_EVENTS.items():
-                        # rules can be a dict or list of dicts
-                        if isinstance(rules, dict):
-                            rules = [rules]
-                        all_candidates = []
-                        for rule in rules:
-                            filters = rule.get('filters', {})
-                            if isinstance(filters, dict):
-                                q = Q(**filters)
-                            elif callable(filters):
-                                q = filters()
-                            elif isinstance(filters, Q):
-                                q = filters
-                            else:
-                                raise TypeError
-                            candidates = model.objects.filter(q)
-                            date_filters = {}
-                            min_timedelta, max_timedelta = rule.get('min_timedelta', None), rule.get('max_timedelta', None)
-                            if min_timedelta:
-                                date_filters[rule['date_field'] + '__gte'] = timezone.now() + min_timedelta
-                            if max_timedelta:
-                                date_filters[rule['date_field'] + '__lte'] = timezone.now() + max_timedelta
-                            candidates = candidates.filter(**date_filters)
-                            #make sure email had not been sent before.
-                            sent_objects_ids = [values[0] for values in model.get_sent_notifications(action).values_list('object_id')]
-                            all_candidates += candidates.exclude(id__in=sent_objects_ids)
+                        all_candidates = model.get_candidates(action)
                         
                         for obj in all_candidates:
                             print obj.send_auto_notifer_email(action)
