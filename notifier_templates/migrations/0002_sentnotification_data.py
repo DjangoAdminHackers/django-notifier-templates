7080aaefb3b2c79cd58c99a9b2c6914b7ad2280b
Andy Baker <andy@andybak.net>
2016-08-15 11:29:54 +0100

    migration was failing without postgres

--- notifier_templates/migrations/0002_sentnotification_data.py
+++ notifier_templates/migrations/0002_sentnotification_data.py
@@ -7,10 +7,14 @@ from django.conf import settings
 
 # This is pretty bad (controlling migrations from the config), but we want to
 # have a way of ensuring JSON fallback for Postgres < 9.4
-if getattr(settings, 'JSONB_DISABLED', False):
-    from django_pgjson.fields import JsonField
-else:
-    from django_pgjson.fields import JsonBField as JsonField
+try:
+    import django_pgjson
+    if getattr(settings, 'JSONB_DISABLED', False):
+        from django_pgjson.fields import JsonField
+    else:
+        from django_pgjson.fields import JsonBField as JsonField
+except ImportError:
+    django_pgjson = None
 
 
 class Migration(migrations.Migration):
@@ -18,12 +22,12 @@ class Migration(migrations.Migration):
     dependencies = [
         ('notifier_templates', '0001_initial'),
     ]
-
-    operations = [
-        migrations.AddField(
-            model_name='sentnotification',
-            name='data',
-            field=JsonField(default={}),
-            preserve_default=False,
-        ),
-    ]
+    if django_pgjson:
+        operations = [
+            migrations.AddField(
+                model_name='sentnotification',
+                name='data',
+                field=JsonField(default={}),
+                preserve_default=False,
+            ),
+        ]
