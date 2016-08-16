66dde6bc5f701755fc0f655274f9e9c4d1412b54
Andy Baker <andy@andybak.net>
2016-08-16 16:14:08 +0100

    Warn but don't fail if tables don't exist

--- notifier_templates/apps.py
+++ notifier_templates/apps.py
@@ -1,5 +1,12 @@
+import logging
+
 from django.apps import AppConfig
+from django.db import DatabaseError
+
 from .utils import generate_all_notifier_templates
+
+
+logger = logging.getLogger(__name__)
 
 
 class NotifierTemplateAppConfig(AppConfig):
@@ -8,4 +15,8 @@ class NotifierTemplateAppConfig(AppConfig):
     verbose_name = 'Notification Templates'
 
     def ready(self):
-        generate_all_notifier_templates()
+        try:
+            generate_all_notifier_templates()
+        except DatabaseError:
+            # During migrations we are called before the table has been created
+            logger.warn("Skipping generate_all_notifier_templates as db table doesn't exist")
